3b3052ba4ff40362dba579fb3ce3486737550ab1
지인성 <crm03008@gmail.com>
2024-03-12 17:46:39 +0900

    S10P22A706-110 feat: 온보딩, 회원가입, 로그인 작업
    - 온보딩 페이지 제작
    - 회원가입 페이지 생성
    - 로그인 페이지 생성
    - 디바운스, 이메일 검증 커스텀 훅 추가
    - 테일윈드 팀 색상 추가
    - 공통 테일윈드 컴포넌트 스타일 추가
    - 버튼, 텍스트 공통 컴포넌트 추가

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,1 +1,3 @@
 export { default as Boarding } from "./onBoarding";
+export { default as Signup } from "./Singup";
+export { default as Signin } from "./Signin";
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -8,6 +8,23 @@ export default {
   darkMode: "class",
   theme: {
     extend: {},
+    colors: {
+      A706Blue: "#F5F7FF",
+      A706Blue2: "#E6EBFF",
+      A706Blue3: "#AAB9FF",
+      A706SubBlue: "#6F87FF",
+      A706CheryBlue: "#5471FF",
+      A706Yellow: "#FFD833",
+      A706Red: "#F25555",
+      A706LightGrey: "#F5F5F5",
+      A706LightGrey2: "#DBDCDC",
+      A706Grey: "#B5B7BA",
+      A706Grey2: "#909398",
+      A706DarkGrey1: "#5C5F66",
+      A706DarkGrey2: "#292C34",
+      A706SlateGrey: "#5D636C",
+      A706Green: "#134F2C",
+    },
   },
   plugins: [require("flowbite/plugin")],
 };

906abdc80d086f82209c6bf41a66421608bfe8bb
지인성 <crm03008@gmail.com>
2024-03-18 17:48:25 +0900

    S10P22A706-183 feat: React Query 설정 추가
    - React Query 프로바이더 추가

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -1,28 +1,33 @@
-import { DarkThemeToggle, Flowbite } from "flowbite-react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Main, Boarding, Signup, Signin, MyPage } from "@/pages";
 import { Header, Footer } from "@/widgets";
 import "./index.css";
+import { DarkThemeToggle, Flowbite } from "flowbite-react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <Flowbite>
-      <div className="Container">
-        <DarkThemeToggle className="absolute right-0 z-10" />
-        <Header />
-        <main className="flex flex-col flex-1">
-          <Router>
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/boarding" element={<Boarding />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/signin" element={<Signin />} />
-              <Route path="/mypage" element={<MyPage />} />
-            </Routes>
-          </Router>
-        </main>
-        <Footer />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="Container">
+          <DarkThemeToggle className="absolute right-0 z-10" />
+          <Header />
+          <main className="flex flex-col flex-1">
+            <Router>
+              <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/boarding" element={<Boarding />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/mypage" element={<MyPage />} />
+              </Routes>
+            </Router>
+          </main>
+          <Footer />
+        </div>
+      </QueryClientProvider>
     </Flowbite>
   );
 };

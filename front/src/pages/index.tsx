8d49fa3adc3afcef35c27f167aa8a41175308cea
DESKTOP-64649GT\Master <crm03008@gmail.com>
2024-03-20 00:51:03 +0900

    S10P22A706-223 feat: reconstruct signup
    - 전체 입력 데이터 변경으로 인한 페이지 재구성
    - 회원 가입 페이지 간소화로 인한 페이지 수정
    - 시설 관리자 등록 분리로 인한 별도 페이지 구성
    - Tailwind CLS 유틸 추가
    - 시설 검색시 좌표 정보를 보내주는 로직 추가
    - 버튼 동작이 유효성 검사를 모두 통과한 경우에만 동작하도록 수정 및
      스타일 추가

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -2,5 +2,6 @@ export { default as Boarding } from "./onBoarding";
 export { default as Main } from "./main";
 export { default as Signup } from "./Singup";
 export { default as Signin } from "./Signin";
+export { default as AgencyRegist } from "./AgencyRegist";
 export { default as FoundItemWrite } from "./foundItemWrite";
 export { default as FoundItemDetail } from "./foundItemDetail";

4331efc6bf9c92ffbadd4304ffd65df0ea4deeff
신문영 <ztrl@naver.com>
2024-03-21 17:47:20 +0900

    S10P22A706-127 feat: Complete foundItemDetail page frame

--- front/src/pages/LostItemRegist/index.tsx
+++ front/src/pages/LostItemRegist/index.tsx
@@ -1,20 +1,61 @@
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
-import { CustomButton } from "@/shared";
+import { CustomButton, KakaoMap } from "@/shared";
 import { useEffect, useState } from "react";
 import { Select, Textarea, TextInput } from "flowbite-react";
 import { ProgressBar } from "@/widgets";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 
 const LostItemRegist = () => {
-  const imageReader = new FileReader();
   const [progress, setProgress] = useState<number>(0);
   //   const [autoFilled, setAutoFilled] = useState<boolean>(false);
   const [question, setQuestion] = useState<JSX.Element>();
   const [inputForm, setInputForm] = useState<JSX.Element>();
-  const [files, setFiles] = useState<File[]>([]);
-  const [uploadedFiles, setUploadedFiles] = useState<FileList>();
-  const [images, setImages] = useState<string[]>([]);
+  const [imageURL, setImageURL] = useState<string>();
+
+  const saveImageFile = (fileList: FileList) => {
+    const file = fileList[0];
+    if (file.type.split("/")[0] === "image") {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setImageURL((reader.result ?? "").toString());
+      };
+    }
+  };
+
+  const renderImageList = () => {
+    if (imageURL && imageURL.length > 0) {
+      return (
+        <div className="flex flex-row justify-center">
+          <img
+            className="bg-white border-dashed border border-black rounded-lg size-[240px]"
+            src={imageURL}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex flex-row justify-center">
+          <div className="flex justify-center">
+            <label
+              htmlFor="file"
+              className="flex flex-col justify-center items-center cursor-pointer bg-white border-dashed border border-black rounded-lg size-[80px] mx-5"
+            >
+              <AddBoxOutlinedIcon />
+            </label>
+            <input
+              id="file"
+              type="file"
+              hidden
+              multiple
+              onChange={(e) => saveImageFile(e.target.files ?? new FileList())}
+            />
+          </div>
+        </div>
+      );
+    }
+  };
 
   const PAGE_ELEMENT: Record<number, any> = {
     0: {
@@ -86,70 +127,33 @@ const LostItemRegist = () => {
     },
     4: {
       question: "물건의 사진이 있다면 올려주세요. 찾는데 도움이 됩니다.",
+      inputForm: <>{renderImageList()}</>,
+    },
+    5: {
+      question: "잃어버린 날짜를 기억하시나요?",
       inputForm: (
         <>
-          {images.map((image, index) => (
-            <img key={index} src={image} />
-          ))}
-          <label
-            htmlFor="file"
-            className="flex flex-col justify-center items-center cursor-pointer bg-white border-dashed border border-black rounded-lg size-[80px] mx-5"
-          >
-            <AddBoxOutlinedIcon />
-          </label>
-          <input
-            id="file"
-            type="file"
-            hidden
-            multiple
-            onChange={(e) => setUploadedFiles(e.target.files ?? new FileList())}
-          ></input>
+          <Select>
+            <option>날짜선택</option>
+          </Select>
         </>
       ),
     },
-    5: {},
-    6: {},
-    7: {},
+    6: {
+      question: "잃어버린 장소도 기억하시나요?",
+      inputForm: (
+        <>
+          <TextInput placeholder="장소를 검색해주세요" />
+          <KakaoMap className="size-[280px] m-5"></KakaoMap>
+        </>
+      ),
+    },
   } as const;
   type PAGE_ELEMENT = (typeof PAGE_ELEMENT)[keyof typeof PAGE_ELEMENT]; // 'iOS' | 'Android'
 
-  imageReader.onloadend = () => {
-    const result = imageReader.result ?? "";
-    setImages((images) => [...images, result.toString()]);
-  };
-
   useEffect(() => {
-    setInputForm(() => (
-      <>
-        {images.map((image, index) => (
-          <img key={index} src={image} />
-        ))}
-        <label
-          htmlFor="file"
-          className="flex flex-col justify-center items-center cursor-pointer bg-white border-dashed border border-black rounded-lg size-[80px] mx-5"
-        >
-          <AddBoxOutlinedIcon />
-        </label>
-        <input
-          id="file"
-          type="file"
-          hidden
-          multiple
-          onChange={(e) => setUploadedFiles(e.target.files ?? new FileList())}
-        ></input>
-      </>
-    ));
-  }, [images]);
-
-  useEffect(() => {
-    files
-      .filter((file) => file.type.split("/")[0] === "image")
-      .forEach((file) => imageReader.readAsDataURL(file));
-  }, [files]);
-
-  useEffect(() => {
-    setFiles((files) => [...files, ...(uploadedFiles ?? [])]);
-  }, [uploadedFiles]);
+    setInputForm(() => <>{renderImageList()}</>);
+  }, [imageURL]);
 
   useEffect(() => {
     setQuestion(() => <p>{PAGE_ELEMENT[progress].question}</p>);
@@ -160,17 +164,15 @@ const LostItemRegist = () => {
     <>
       <div className="flex flex-col justify-center self-center w-[360px]">
         <div className="h-[48px]">
-          <CustomButton
-            children={
-              <ArrowBackIosNewIcon
-                onClick={() =>
-                  setProgress((progress) =>
-                    progress > 0 ? progress - 1 : progress
-                  )
-                }
-              />
-            }
-          ></CustomButton>
+          <CustomButton>
+            <ArrowBackIosNewIcon
+              onClick={() =>
+                setProgress((progress) =>
+                  progress > 0 ? progress - 1 : progress
+                )
+              }
+            />
+          </CustomButton>
         </div>
         <div className="flex justify-center place-items-center h-[48px]">
           <ProgressBar progress={progress} />
@@ -179,7 +181,9 @@ const LostItemRegist = () => {
           <div className="flex flex-col text-center justify-center align-middle h-[352px]">
             {question}
           </div>
-          <div className="flex justify-center h-auto">{inputForm}</div>
+          <div className="flex flex-col justify-start h-[352px]">
+            {inputForm}
+          </div>
         </div>
         <div className="flex justify-center h-[48px]">
           <CustomButton

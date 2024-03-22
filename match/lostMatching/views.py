775df700d716206a821246e204d571b24b568dd1
SYH <syhuni0314@gmail.com>
2024-03-22 15:02:49 +0900

    S10P22A706-207 feat: request 처리 구조 구현

--- match/lostMatching/views.py
+++ match/lostMatching/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 
 def lost_matching(request):
+    if request.method == 'POST':
+        # try:
+        #     body = json.loads(request.body)
+        # except JSONDecodeErorr:
+        #     return JsonResponse({'error':'invalid json'}, status=400)
+        body = json.loads(request.body)
     return JsonResponse({'message':'success'}, status = 200)

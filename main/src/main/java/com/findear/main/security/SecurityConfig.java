9e1bbf3e7f9f5c765c45c2eb14c8562141505556
Ehigh <rkd2rb@naver.com>
2024-03-22 22:03:21 +0900

    test: CORS allowed origin * 로 변경(테스트용)

--- main/src/main/java/com/findear/main/security/SecurityConfig.java
+++ main/src/main/java/com/findear/main/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);

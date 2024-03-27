48f1f0fabce5b20c3c0e86a86d5ffa5ac5f4d270
SYH <syhuni0314@gmail.com>
2024-03-27 14:35:04 +0900

    S10P22A706-207 feat: findear 매칭 api 구현

--- match/lostMatching/urls.py
+++ match/lostMatching/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
         # path('', views.index, name='index'),
         path('matching/lost', views.lost_matching, name='lost-matching'),
-        # path('matching/findear', views.findear_matching, name='findear-matching'),
+        path('matching/findear', views.findear_matching, name='findear-matching'),
         # path('process', views.process, name='process'),
 ]
--- match/lostMatching/views.py
+++ match/lostMatching/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import pandas as pd
+from numpyencoder import NumpyEncoder
 
 # Create your views here.
 
@@ -20,10 +21,57 @@ def process_lost_item_data(found_item_info):
     df = pd.DataFrame([found_item_info])
 
     # 데이터 처리 수행
-    processed_data = analyze_data(df)
+    processed_data = analyze_lost_data(df)
 
     return processed_data
 
-def analyze_data(data):
+def analyze_lost_data(data):
     # 데이터 분석 수행
     return data
+
+def findear_matching(request):
+    if request.method == 'POST':
+        # try:
+        #     body = json.loads(request.body)
+        # except JSONDecodeErorr:
+        #     return JsonResponse({'error':'invalid json'}, status=400)
+        body = json.loads(request.body)
+        # print(body)
+        data = process_findear_item_data(body)
+        # print(data)
+    return JsonResponse({ 'message':'success', 'result':data }, status = 200)
+    # return JsonResponse({ 'message':'success', 'result':body }, status = 200)
+
+def process_findear_item_data(found_item_info):
+    # 받은 데이터를 이용하여 DataFrame 생성
+    df = pd.DataFrame([found_item_info])
+    
+    print(df)
+
+    # 데이터 처리 수행
+    processed_data = analyze_findear_data(df)
+    
+    # json.dumps(processed_data, cls=NumpyEncoder, ensure_ascii=False)
+
+    return processed_data
+
+def analyze_findear_data(data):
+    # 데이터 분석 수행
+    data_list = [
+		{
+			"lostBoardId" : 1,
+			"acquiredBoardId" : 1,
+			"simulerityRate" : 0.9
+		},
+		{
+			"lostBoardId" : 1,
+			"acquiredBoardId" : 2,
+			"simulerityRate" : 0.8
+		},
+		{
+			"lostBoardId" : 1,
+			"acquiredBoardId" : 5,
+			"simulerityRate" : 0.9
+		}
+	]
+    return data_list

d06fdbf7dd4e51d2a79a65ad307acc8615fc8c22
Zerotay <donggun1000@naver.com>
2024-04-01 10:44:19 +0900

    S10P22A706-206 feat: make calColor method

--- match/lostMatching/models.py
+++ match/lostMatching/models.py
@@ -13,15 +13,30 @@ from dotenv import load_dotenv
 import os
 import time
 import logging
+
+from selenium import webdriver 
+from selenium.webdriver.common.by import By # find_element 함수 쉽게 쓰기 위함
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import random
 # Create your models here.
 
 logger = logging.getLogger(__name__)
 class matchModel():
     
     def __init__(self) -> None:
+        # load model
         load_dotenv()
         path = os.getenv("MODEL_PATH")
         #model = fasttext.load_model(path)
+
+        # open color crawling
+        self.webPath = 'http://web.kats.go.kr/KoreaColor/color.asp'
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.webPath)
+        self.timeToWait = 0.001
+
         return None
 
     def setData(self, lost, found):
@@ -51,17 +66,75 @@ class matchModel():
         elif self.source == 'lost112':
             pass
 
+    def calColor(self):
+        self.score['color'] = 0
+        lostColor = self.getColor(self.lost['color'])
+        if lostColor is None: return None
+        lostRGB = self.getCodeFromColor(lostColor)
+        npLst = np.array([])
+
+        for i in self.found['color']:
+            foundColor = self.getColor(i)
+            if foundColor is None:
+                np.append(npLst,[[255]])
+                continue
+            foundRGB = self.getCodeFromColor(foundColor)
+            colorScore = sum( [w*((l-f)**2) for w,l,f in zip([0.3,0.59,0.11], lostRGB, foundRGB)])
+            np.append(npLst,[[colorScore]])
+
+        minmaxScaler = MinMaxScaler().fit([[0],[255]])
+        X_train_minmax = minmaxScaler.transform(npLst)
+        nplst = 1 - np.array(X_train_minmax).squeeze()
+
+        self.score['color'] = nplst
+        return
     
-    def loadFastText(self):
+    def cal(self):
         pass
     
-    def loadColor(self):
-        pass
 
     def test(self):
         print('teststest')
         return 'test'
 
+    def getColor(self, query):
+        '''
+        None 반환 시 색 계산 제외
+        '''
+        if query == '':
+            return None
+        elif len(query) == 1:
+            query = query+'색' 
+        inputBox = self.driver.find_element(By.XPATH,'/html/body/form/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[1]/td[1]/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr/td[2]/input' )
+        inputBox.clear()
+        inputBox.send_keys(query)
+        inputBox.send_keys(Keys.RETURN)
+        self.driver.implicitly_wait(self.timeToWait)
+
+        colorBox = self.driver.find_element(By.XPATH, '/html/body/form/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[1]/td[1]/table/tbody/tr/td[3]/table/tbody/tr[6]/td/div/select')
+        colorLst = colorBox.text.split('\n')
+        if len(colorLst) == 1:
+            index = 0
+        elif len(colorLst) == 0:
+            return None
+        elif query in colorLst:
+            index = colorLst.index(query)
+        else:
+            index = random.randint(0, len(colorLst))
+
+        select = Select(colorBox)
+        select.select_by_index(index)
+        selected_option = select.first_selected_option
+        action = ActionChains(self.driver)
+        action.double_click(selected_option).perform()
+        self.driver.implicitly_wait(self.timeToWait)
+
+        rgb = self.driver.find_element(By.NAME, 'html_text').get_attribute('value')
+        return rgb
+    
+    def getCodeFromColor(self, color):
+        rgb = [int(color[i:i+1], base=16) for i in range(0,6,2)]
+        return rgb
 
 if __name__ == '__main__':
     print('if selected')
